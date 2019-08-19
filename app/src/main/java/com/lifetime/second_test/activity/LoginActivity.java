cd220fc9308913a11b699a3a49e442ed00019ec6
sonminh162 <32251649+sonminh162@users.noreply.github.com>
2019-08-19 17:13:22 +0700

    'fix-comment'

--- app/src/main/java/com/lifetime/second_test/activity/LoginActivity.java
+++ app/src/main/java/com/lifetime/second_test/activity/LoginActivity.java
@@ -1,23 +1,25 @@
 package com.lifetime.second_test.activity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.lifetime.second_test.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
+
+    public static final List<String> listMember = new ArrayList<String>();
+    public static final int REGISTERED_DATA = 1;
+    public static final int RESULT = 1;
 
     TextView signUpLink;
     TextView forgotButton;
@@ -28,8 +30,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        customActionBar();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scrollable_login);
 
@@ -39,24 +39,26 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.password_input);
 
         Intent intent = getIntent();
-        Bundle bundle = intent.getBundleExtra("RegisteredData");
-        final String emailAddressReceived = bundle.getString("EmailAddress");
-        final String passwordReceived = bundle.getString("Password");
-
-        email.setText(emailAddressReceived);
-        password.setText(passwordReceived);
+        final Bundle bundle = intent.getBundleExtra("RegisteredData");
+        email.setText("");
+        password.setText("");
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean emailValid = email.getText().toString().equals(emailAddressReceived);
-                boolean passwordValid = password.getText().toString().equals(passwordReceived);
-                if (emailValid && passwordValid) {
+                boolean userValid;
+                String getEmailInput = email.getText().toString();
+                String getPasswordInput = password.getText().toString();
+                userValid = listMember.contains(getEmailInput + getPasswordInput);
+
+                if (userValid) {
                     Intent intent = new Intent(LoginActivity.this, NewFeedActivity.class);
                     startActivity(intent);
+                    finish();
                 } else {
                     Toast.makeText(LoginActivity.this, "Failed", Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
 
@@ -64,7 +66,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, REGISTERED_DATA);
             }
         });
 
@@ -79,29 +81,24 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void customActionBar() {
-        //make translucent statusBar on kitkat devices
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (data == null) {
+            return;
         }
-        if (Build.VERSION.SDK_INT >= 19) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        //make fully Android Transparent Status bar
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
 
-    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
-        Window win = activity.getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
+        if (requestCode == REGISTERED_DATA) {
+            Bundle bundle = data.getBundleExtra("RegisteredData");
+            String emailGiveBack = bundle.getString("EmailAddress");
+            String passwordGiveBack = bundle.getString("Password");
+            listMember.add(emailGiveBack + passwordGiveBack);
+
+            if (resultCode == RESULT) {
+                email.setText(emailGiveBack);
+                password.setText(passwordGiveBack);
+            }
         }
-        win.setAttributes(winParams);
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/lifetime/second_test/activity/RegisterActivity.java
+++ app/src/main/java/com/lifetime/second_test/activity/RegisterActivity.java
@@ -1,24 +1,16 @@
 package com.lifetime.second_test.activity;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.lifetime.second_test.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    public static final int REGISTERED_DATA = 1;
 
     TextView signUpButton;
     TextView signIn;
@@ -29,8 +21,6 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        customActionBar();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scrollable_register);
 
@@ -40,65 +30,47 @@ public class RegisterActivity extends AppCompatActivity {
         password = findViewById(R.id.password_value_am2);
         emailAddress = findViewById(R.id.email_address_value_sign_in);
 
-        final String emailAddressForIntent = emailAddress.getText().toString();
-        final String passwordForIntent = password.getText().toString();
+//        final String emailAddressForIntent = emailAddress.getText().toString();
+//        final String passwordForIntent = password.getText().toString();
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveDataToIntent(emailAddressForIntent, passwordForIntent);
-                openDialogShowResult();
+                if (validateSimple()) {
+                    saveDataToIntent(emailAddress.getText().toString(), password.getText().toString(), LoginActivity.RESULT);
+                }
             }
         });
     }
 
-    private void saveDataToIntent(String emailAddressForIntent, String passwordForIntent) {
+    public boolean validateSimple() {
+        boolean flag = true;
+        String user = emailAddress.getText().toString();
+        String passwordCheck = password.getText().toString();
+        if (user.isEmpty()) {
+            if (passwordCheck.isEmpty()) {
+                Toast.makeText(this, "user and password not empty!", Toast.LENGTH_SHORT).show();
+                flag = false;
+            } else {
+                Toast.makeText(this, "user not empty!", Toast.LENGTH_SHORT).show();
+                flag = false;
+            }
+        } else {
+            if (passwordCheck.isEmpty()) {
+                Toast.makeText(this, "password not empty", Toast.LENGTH_SHORT).show();
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    private void saveDataToIntent(String emailAddressForIntent, String passwordForIntent, int resultCode) {
         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
         Bundle bundle = new Bundle();
         bundle.putString("EmailAddress", emailAddressForIntent);
         bundle.putString("Password", passwordForIntent);
         intent.putExtra("RegisteredData", bundle);
-        startActivityForResult(intent,REGISTERED_DATA);
-    }
-
-    private void openDialogShowResult() {
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle(R.string.title_dialog);
-        alertDialog.setMessage(getResources().getString(R.string.message_dialog));
-
-        alertDialog.setButton(getResources().getString(R.string.next_activity), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-        alertDialog.show();
-    }
-
-    private void customActionBar() {
-        //make translucent statusBar on kitkat devices
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        //make fully Android Transparent Status bar
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
-
-    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
-        Window win = activity.getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
-        }
-        win.setAttributes(winParams);
+        setResult(resultCode, intent);
+        finish();
     }
 }
